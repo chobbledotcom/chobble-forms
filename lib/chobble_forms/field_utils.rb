c56c1486a3f331ec7d35193da221e776edec8476
Stefan <git@stefn.co.uk>
2025-08-15 19:01:11 +0100

    Support enum pass/fail/NA better

--- lib/chobble_forms/field_utils.rb
+++ lib/chobble_forms/field_utils.rb
@@ -17,9 +17,7 @@ module ChobbleForms
       fields = T.let([], T::Array[Symbol])
       partial_str = partial.to_s
 
-      if partial_str.include?("pass_fail") && !field.to_s.end_with?("_pass")
-        fields << :"#{field}_pass"
-      end
+      fields << :"#{field}_pass" if partial_str.include?("pass_fail") && !field.to_s.end_with?("_pass")
 
       if partial_str.include?("comment")
         base = field.to_s.end_with?("_pass") ? strip_field_suffix(field) : field
@@ -52,6 +50,11 @@ module ChobbleForms
     sig { params(form: Symbol, field: Symbol).returns(String) }
     def self.form_field_label(form, field)
       I18n.t("forms.#{form}.fields.#{field}")
+    end
+
+    sig { params(assessment_key: Symbol).returns(Symbol) }
+    def self.form_name_from_assessment(assessment_key)
+      assessment_key.to_s.gsub(/_assessment$/, "").to_sym
     end
   end
 end
