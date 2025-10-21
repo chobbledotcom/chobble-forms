54cf2eb10471992e224876678a220cddff86fd30
stefan burke <83819576+stefan-burke@users.noreply.github.com>
2025-10-21 01:24:23 +0000

    Add searchable native HTML combobox support (#9)

--- spec/views/chobble_forms/_select.html.erb_spec.rb
+++ spec/views/chobble_forms/_select.html.erb_spec.rb
@@ -180,6 +180,7 @@ RSpec.describe "chobble_forms/_select.html.erb", type: :view do
       allow(mock_form).to receive(:text_field)
         .with(field, {required: false, list: "status_datalist_123"})
         .and_return('<input type="text" list="status_datalist_123" />'.html_safe)
+      allow(mock_form).to receive(:select)
     end
 
     it "renders a text field with datalist when combobox is true" do
@@ -280,9 +281,7 @@ RSpec.describe "chobble_forms/_select.html.erb", type: :view do
   end
 
   describe "different field types" do
-    let(:different_fields) { [:manufacturer, :model, :location] }
-
-    different_fields.each do |field_name|
+    [:manufacturer, :model, :location].each do |field_name|
       context "with field #{field_name}" do
         before do
           allow(view).to receive(:form_field_setup).and_return(
